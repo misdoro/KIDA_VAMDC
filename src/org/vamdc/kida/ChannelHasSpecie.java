51943a8c9e7bf99a2e256f39c98fece5f1bada29
Benjamin Pavone <ben00ze@gmail.com>
2011-09-19 18:27:59 +0200

    Misha's commit

--- src/org/vamdc/kida/ChannelHasSpecie.java
+++ src/org/vamdc/kida/ChannelHasSpecie.java
@@ -4,4 +4,6 @@ import org.vamdc.kida.auto._ChannelHasSpecie;
 
 public class ChannelHasSpecie extends _ChannelHasSpecie {
 
+	public final static String PRODUCT="product";
+	public final static String REACTANT="reactant";
 }
