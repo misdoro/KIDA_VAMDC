244108855690a490f4453e0be9fb50b6e9e43c20
Misha Doronin <misha@doronin.org>
2012-04-24 14:38:01 +0200

    Some small cleanups in Specie extension

--- src/main/java/org/vamdc/kida/dao/Specie.java
+++ src/main/java/org/vamdc/kida/dao/Specie.java
@@ -22,7 +22,7 @@ public class Specie extends _Specie {
 		for(SpecieHasElement she : tabShe )
 		{
 			tm.put(she.getElement().getSymbol(), she.getOccurrence() );
-			
+
 		}
 		for (Entry<String,Integer>me: tm.entrySet())
 		{
@@ -31,14 +31,14 @@ public class Specie extends _Specie {
 				formula += me.getValue().toString();
 
 		} 
-		
+
 		if ( this.getCharge() < 0 )
 			formula += "-";
 		if ( this.getCharge() > 0 )
 			formula += "+";
 		return formula;
 	}
-	
+
 	public String getCommonNameLatex()
 	{
 		String formula = "";
@@ -47,12 +47,12 @@ public class Specie extends _Specie {
 		for(SpecieHasElement she : tabShe )
 		{
 			tm.put(she.getElement().getSymbol(), she.getOccurrence() );
-			
+
 		}
-		
+
 		for (Entry<String,Integer>me: tm.entrySet())
 		{
-			
+
 			if ( (Integer) me.getValue() > 1 )
 			{
 				formula += me.getKey().toString() + "$_" + me.getValue().toString();
@@ -61,18 +61,18 @@ public class Specie extends _Specie {
 			{
 				formula += me.getKey().toString();
 			}
-				
+
 
 		} 
-		
+
 		if ( this.getCharge() < 0 )
 			formula += "$^" + "-";
 		if ( this.getCharge() > 0 )
 			formula += "$^" + "+";
 		return formula;
-		
+
 	}
-	
+
 	public double getMass()
 	{
 		double mass = 0;
@@ -80,14 +80,14 @@ public class Specie extends _Specie {
 		for(SpecieHasElement she : tabShe )
 		{
 			mass += she.getElement().getMassValue() * she.getOccurrence();
-			
+
 		}
-	 return mass;	
+		return mass;	
 	}
-	
+
 	public Integer getNuclearCharge()
 	{
-		
+
 		if ( ! this.isAnAtom()) 
 			return null;
 		List<SpecieHasElement> tabShe = this.getSpecieHasElements();
@@ -97,9 +97,9 @@ public class Specie extends _Specie {
 				return she.getElement().getAtomicNumber(); 	
 		}
 		return null;
-		
+
 	}
-	
+
 	public boolean isASpecialSpecies()
 	{
 		Arrays.sort(specialSpecies);
@@ -107,31 +107,27 @@ public class Specie extends _Specie {
 			return false;
 		int index = Arrays.binarySearch(specialSpecies,this.getFormula() );
 
-		if ( index >= 0 )
-			return true;
-		return false;
-		
+		return index >= 0;
+
 	}
-	
+
 	public boolean isAnAtom()
 	{
-		if (this.getCharge() != 0 )
+		if (this.getCharge() == null || this.getCharge() != 0 )
 			return false;
-		
+
 		if ( this.isASpecialSpecies() )
 			return false;
-		int occurence = 0;
-		List<SpecieHasElement> tabShe = this.getSpecieHasElements();
-		for(SpecieHasElement she : tabShe )
+		
+		if (this.getSpecieHasElements().size()>1)
+			return false;
+		
+		for(SpecieHasElement she : this.getSpecieHasElements())
 		{
-			occurence += she.getOccurrence();
-			if ( occurence > 1 )
-				return false;
-			
+			return she.getOccurrence()==1;
 		}
-		if ( occurence == 1 )
-			return true;
+		
 		return false;
 	}
-	
+
 }
