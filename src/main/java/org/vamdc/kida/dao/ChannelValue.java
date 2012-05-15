d1bf65ca472e3a1fd3d88b305eb5f19c1e7a65ea
Misha Doronin <misha@doronin.org>
2012-05-15 17:12:50 +0200

    Get fancy accuracy string

--- src/main/java/org/vamdc/kida/dao/ChannelValue.java
+++ src/main/java/org/vamdc/kida/dao/ChannelValue.java
@@ -17,6 +17,25 @@ public class ChannelValue extends _ChannelValue {
 		throw new IllegalArgumentException("Value for variable "+variableName+" not found in channel "+this.getChannel().getId());	
 	}
 	
+	public String getAccuracy(){
+		StringBuilder result = new StringBuilder();
+		result.append("Accuracy: ");
+		String unc1="";
+		String unc2="";
+		
+		for (ChannelVariable var: this.getChannelVariables()){
+			if ("alpha".equals(var.getVariableName().getName()))
+				unc1=var.getTypeUncert()+" F_0="+var.getUncert();
+			else if ("beta".equals(var.getVariableName().getName())
+					&& var.getUncert()!=null){
+				unc2=" g="+var.getUncert();
+			}
+		}
+		result.append(unc1).append(unc2).append(", see http://kida.obs.u-bordeaux1.fr/help/helpUncert for more info");
+		return result.toString();
+		
+	}
+	
 	public boolean isValid() {
 		if (getIsTrash() == 1)
 			return false;
